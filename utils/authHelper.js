9333d4a53bdff991a4837d6e5a270ddbbe9489f9
Rohit K <Rohit K>
2025-06-28 22:56:53 +0530

    Fixed issues

--- utils/authHelper.js
+++ utils/authHelper.js
@@ -7,9 +7,8 @@ const {createAuthorizationHeader} = require("ondc-crypto-sdk-nodejs");
 
 async function createAuthorizationHeaderONDC(requestBody) {
     try {
-       
         const header = await createAuthorizationHeader({
-            body: requestBody.body,
+            body: requestBody,
             privateKey: process.env.SIGNING_PRIVATE_KEY,
             subscriberId: process.env.SUBSCRIBER_ID, // Subscriber ID that you get after registering to ONDC Network
             subscriberUniqueKeyId: process.env.UNIQUE_KEY_ID, // Unique Key Id or uKid that you get after registering to ONDC Network
